a9824f19f3b522caa1bb0cf447e4962a19078bd4
Dtroonn <Dtroonn@mail.ru>
2021-05-18 16:28:01 +0300

    enable trust proxy

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('trust proxy', 1);
+app.enable('trust proxy');
 app.use(
     session({
         secret: SESSION_SECRET,
